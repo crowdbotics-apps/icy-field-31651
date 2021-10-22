b32a1436debcee13c4542c93a4ffd90d4b896f4d
Crowdbotics <team@crowdbotics.com>
2021-10-22 18:00:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "icy_field_31651",
+  url: "https://icy_field_31651.botics.co",
+  api: "https://icy_field_31651.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
